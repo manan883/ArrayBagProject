3843b4ffbf6e0776299b10c35b0a458d94c3956d
manan <manan883@gmail.com>
2022-02-12 19:50:32 -0800

    more GUI changes

--- src/client.java
+++ src/client.java
@@ -7,17 +7,24 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 public class client {
-
+private static JFrame main = new JFrame();
+private static JFrame linkedF = new JFrame();
+private static JFrame resizeF = new JFrame();
+private static JFrame input = new JFrame();
+private static JFrame error = new JFrame();
+private static String userText = "";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//have user choose which type they want then call methods based on that 
 		GUI();
 	}
+	
+	//main GUI
 	public static void GUI() {
 		//set frame
 		 JFrame f = new JFrame("GUI");
-		 f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 f.setSize(500,500);
+		 main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 main.setSize(500,500);
 		 
 		 //Individual buttons and other stuff
 		 JPanel welcome = new JPanel();
@@ -30,6 +37,7 @@ public class client {
 		 linked.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){  
 		    GUI_L();
+		    main.dispose();
 		 }  
 }); 
 		 
@@ -40,46 +48,74 @@ public class client {
 		 
 		 
 		 //frame stuff
-		 f.getContentPane().add(BorderLayout.CENTER, options);
-		 f.getContentPane().add(BorderLayout.NORTH, welcome);
-		 f.setVisible(true);
+		 main.getContentPane().add(BorderLayout.CENTER, options);
+		 main.getContentPane().add(BorderLayout.NORTH, welcome);
+		 main.setVisible(true);
 	}
+	
+	//main GUI linked
 	public static void GUI_L() {
 		//set up frames
-		JFrame f = new JFrame("Linked");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(500,500);
+//		JFrame f = new JFrame("Linked");
+		linkedF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		linkedF.setSize(500,500);
 		
 		//Individual buttons and stuff
 		JPanel options = new JPanel();
 		JLabel l = new JLabel("This is for the LinkedData Type, options shown below");
 		JButton b = new JButton("Back");
-		
+		JPanel choices = new JPanel();
+		JButton makeNewBag = new JButton("Make a new bag");
+		JButton add = new JButton("Add Element to a bag");
+		JButton remove = new JButton("Remove an Element from a Bag");
+		JButton del = new JButton("Delete a bag");
+		JButton list = new JButton("List the contents of a bag");
+		JButton union = new JButton("Combine 2 bags into a new one");
+		JButton intersect = new JButton("Intersect 2 bags");
+		JButton difference = new JButton("Get the Diffence of 2 bags");
 		
 		//actions
 		b.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){  
 		    GUI();
+		    linkedF.dispose();
 		 }  
-}); 
+}); //end event b
+		//start event makenewbag
+		makeNewBag.addActionListener(new ActionListener(){  
+		    public void actionPerformed(ActionEvent e){  
+		    	GUI_UI("Enter the name of the new bag: ", "Done", "linked");
+		    	System.out.println(userText);
+		    	
+		 }  
+}); //end event makenewbag
 		
 		//panel stuff
 		options.add(b);
 		options.add(l);
+		choices.add(makeNewBag);
+		choices.add(del);
+		choices.add(add);
+		choices.add(remove);
+		choices.add(list);
+		choices.add(union);
+		choices.add(intersect);
+		choices.add(difference);
 		
 		//frame stuff
-		f.getContentPane().add(BorderLayout.NORTH, options);
-		f.setVisible(true);
+		linkedF.getContentPane().add(BorderLayout.NORTH, options);
+		linkedF.getContentPane().add(BorderLayout.CENTER, choices);
+		linkedF.setVisible(true);
 		
 	}
 	
 	
-	
+	//main GUI resize
 	public static void GUI_R() {
 		//set up frames
-		JFrame f = new JFrame("Linked");
-		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		f.setSize(500,500);
+//		JFrame f = new JFrame("Linked");
+		resizeF.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		resizeF.setSize(500,500);
 		
 		//Individual buttons and stuff
 		JPanel options = new JPanel();
@@ -91,6 +127,7 @@ public class client {
 		b.addActionListener(new ActionListener(){  
 		    public void actionPerformed(ActionEvent e){  
 		    GUI();
+		    resizeF.dispose();
 		 }  
 }); 
 		
@@ -99,12 +136,121 @@ public class client {
 		options.add(l);
 		
 		//frame stuff
-		f.getContentPane().add(BorderLayout.NORTH, options);
-		f.setVisible(true);
+		resizeF.getContentPane().add(BorderLayout.NORTH, options);
+		resizeF.setVisible(true);
+	}
+	
+	//universal error message pop up
+	public static void errorMessages(String message, String gui) {
+		error.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		error.setSize(200,200);
+		JPanel p = new JPanel();
+		JLabel l = new JLabel(message);
+		JButton back = new JButton("OK");
+		
+		if(gui.equals("linked")) {
+			back.addActionListener(new ActionListener(){  
+			    public void actionPerformed(ActionEvent e){  
+			    GUI_L();
+			    error.dispose();
+			 }  
+	}); //end action
+		}
+		else if(gui.equals("resize")) {
+			back.addActionListener(new ActionListener(){  
+			    public void actionPerformed(ActionEvent e){  
+			    GUI_R();
+			    error.dispose();
+			 }  
+	}); //end action
+		}
+		
+		p.add(l);
+		p.add(back);
+		error.getContentPane().add(BorderLayout.CENTER, p);
+		error.setVisible(true);
+	}
+	//User input, takes string as input, universal
+	public static void GUI_UI (String label, String button, String gui) {
+		input.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		input.setSize(500,200);
+		JPanel topP = new JPanel();
+		JPanel botP = new JPanel();
+		JLabel l = new JLabel(label);
+		JButton cont = new JButton(button);
+		JButton back = new JButton("back");
+		JTextField tf = new JTextField("",30);
+		//panel stuff
+		topP.add(back);
+		topP.add(l);
+		botP.add(tf);
+		botP.add(cont);
+		
+		//frame stuff
+		input.getContentPane().add(BorderLayout.NORTH, topP);
+		input.getContentPane().add(BorderLayout.SOUTH, botP);
+		input.setVisible(true);
+		
+		//actions
+		if(gui.equals("linked")) {
+			System.out.println("test");
+			back.addActionListener(new ActionListener(){  
+			    public void actionPerformed(ActionEvent e){  
+			    GUI_L();
+			    input.dispose();
+			 }  
+	}); //end action
+			cont.addActionListener(new ActionListener(){  
+			    public void actionPerformed(ActionEvent e){  
+			    if((tf.getText() == null) || (tf.getText().trim().isEmpty())){
+			    errorMessages("Theres nothing in the bag","linked");
+			}
+			    else {
+					 userText = tf.getText();
+					 GUI_L();
+					 input.dispose();
+					 
+
+			    }
+			   
+			 }
+	}); //end action
+				//return userText;
+			
+			
+		}
+		
+		
+		else if(gui.equals("resize")) {
+			back.addActionListener(new ActionListener(){  
+			    public void actionPerformed(ActionEvent e){  
+			    GUI_R();
+			    input.dispose();
+			 }  
+	}); //end action
+			cont.addActionListener(new ActionListener(){  
+			    public void actionPerformed(ActionEvent e){  
+			    	if((tf.getText() == null) || (tf.getText().trim().isEmpty())){
+					    errorMessages("Theres nothing in the bag","linked");
+					}
+			    	else {
+			    		userText = tf.getText();
+			    	    GUI_R();
+					    input.dispose();
+			    	}
+			
+			 }  
+	}); //end action
+			//return userText;
+		}
+		else {
+			//return "";
+		}
+
 	}
 	
 	
-	
+	//archive method, console commands
 	public static void Start() {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("What type of bag do you want: \n1:LinkedBag\n2:ResizeableArrayBag\nType 1 or 2");
